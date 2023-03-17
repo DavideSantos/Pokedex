ea493281e0de85db0c8a6f2d238c44b29a092152
Davide Santos <davidesantos91@gmail.com>
2023-03-17 21:12:43 +0100

    remove bootstrap(CDN) add tailwindcss(NPM)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 export default function App() {
   return (
     <>
+      <h1 className="underline font-bold text-3xl">ciaoooooooooooooo</h1>
       <Navbar />
       <Header />
     </>
