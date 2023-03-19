c4b0b698412cf4a3049c98d31abdfc3fbb3300d9
Davide Santos <davidesantos91@gmail.com>
2023-03-19 23:18:19 +0100

    add fetch pokemon e card generata

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,39 @@
-export default function Card() {
+export default function Card(props) {
+  console.log(props);
   return (
     <>
-      <div class="max-w-2xl mx-auto mt-2">
-        <div class="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <img
-              class="rounded-t-lg"
-              src="https://flowbite.com/docs/images/blog/image-1.jpg"
-              alt=""
-            />
-          </a>
-          <div class="p-5">
-            <a href="#">
-              <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-                Titolo
-              </h5>
-            </a>
-            <p class="font-normal text-gray-700 mb-3 dark:text-gray-400">
-              Descrizione
+      <div className="max-w-2xl mx-auto mt-2">
+        <div className="bg-white shadow-2xl border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+          <img
+            className="rounded-t-lg mx-auto"
+            src={props.pokemon.sprites.front_default}
+            alt=""
+          />
+          <div className="p-5">
+            <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white capitalize">
+              Nome: {props.pokemon.name}
+            </h5>
+            <p className="font-normal text-gray-700 mb-3 dark:text-gray-400 capitalize">
+              Ordine nel Pokèdex: {props.pokemon.order} posto
+            </p>
+            <p className="font-normal text-gray-700 mb-3 dark:text-gray-400 capitalize">
+              Peso: {props.pokemon.weight} kg
+            </p>
+            <p className="font-normal text-gray-700 mb-3 dark:text-gray-400 capitalize">
+              Tipo di Pokèmon: {props.pokemon.types[0].type.name}
             </p>
             <a
               href="#"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Dettagli
               <svg
-                class="-mr-1 ml-2 h-4 w-4"
+                className="-mr-1 ml-2 h-4 w-4"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
+                <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path>
               </svg>
             </a>
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
 // Componente header attaccato al body
 
 import Card from "./Card";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    const url = "https://pokeapi.co/api/v2/pokemon/raichu";
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setPokemon(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <section className="container mx-auto h-screen bg-slate-400 flex mt-12">
-        <Card />
+        <Card pokemon={pokemon} />
       </section>
     </>
   );
