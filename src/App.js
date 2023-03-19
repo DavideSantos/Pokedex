23c6d6d226ff9e628620ec7e9252fb22928a7607
Davide Santos <davidesantos91@gmail.com>
2023-03-19 15:36:02 +0100

    creazione navbar fittizzia

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/Navbar";
 export default function App() {
   return (
     <>
-      <h1 className="underline font-bold text-3xl">ciaoooooooooooooo</h1>
       <Navbar />
       <Header />
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,21 @@
 export default function Navbar() {
   return (
     <>
-      <h1>componente navbar</h1>
+      <nav className="bg-slate-900 w-full h-12 flex">
+        <div className="container mx-auto flex items-center justify-between p-4 w-auto gap-x-6">
+          <div className="flex items-center flex-shrink-0">
+            <img
+              className="h-4 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+          </div>
+          <div className="text-white">Home</div>
+          <div className="text-white">Servizi</div>
+          <div className="text-white">Contattaci</div>
+          <div className="text-white">Accedi/registrati</div>
+        </div>
+      </nav>
     </>
   );
 }
